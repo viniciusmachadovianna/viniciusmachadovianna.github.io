a55bfb7ac50375f99fbb90532f70e0220d2e9ce4
Vinícius Machado <viniciusmachadovianna@outlook.com>
2024-11-14 14:02:27 -0300

    loop stripe + assets

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-// GENERAL:
 function $(el) {return document.querySelector(el);}
-// function ev(ev) {return document.addEventListener(ev);}
+const arrow = $("#cursor");
 
-$("#cursor").style.display = "block";
+arrow.style.display = "block";
 document.addEventListener("click",(e) => {
     document.querySelectorAll('*').forEach((element) => {
         element.style.cursor = 'none';
     });
 })
 document.addEventListener('mousemove', (e) => {
-    const arrow = $("#cursor");
     arrow.style.left = `${e.pageX - arrow.clientWidth/2}px`;
     arrow.style.top = `${e.pageY - arrow.clientHeight/2}px`;
 })
@@ -17,12 +15,12 @@ document.addEventListener('mousemove', (e) => {
 const cursor = {
     up: '0,0 25,75 75,75 35,60 20,20 60,35 60,60 35,60 75,75 75,25',
     down: '0,0 75,25 75,75 25,75',
-    link: '60,0 80,20 35,20 80,65 65,80 20,35 20,80 0,60 0,0)'
+    link: '60,0 80,20 35,20 80,65 65,80 20,35 20,80 0,60 0,0'
     // old link: '80,0 100,20 35,20 100,85 85,100 20,35 20,100 0,80 0,0)'
     // arrow: '0,0 100,50 70,60 50,50 60,70 50,100';
 }
 function changeCursor(state = 'up'){
-    $('#cursor').querySelector("polygon").setAttribute("points",cursor[state] || cursor.up);
+    arrow.querySelector("polygon").setAttribute("points",cursor[state] || cursor.up);
 }
 document.addEventListener('mouseup', () => changeCursor('up'));
 document.addEventListener('mousedown', () => changeCursor("down"));
@@ -30,7 +28,6 @@ document.querySelectorAll('a').forEach(a => {
     a.addEventListener('mouseover', () => changeCursor('link'));
     a.addEventListener('mouseout', () => changeCursor('up'));
 });
-// LANGUAGE:
 const langData = {
     en: {
         greeting: 'Hello!',
@@ -61,11 +58,15 @@ const userLang = (navigator.language || navigator.userLanguage).slice(0, 2);
 const language = userLang === 'en' || userLang === 'pt' ? userLang : 'en';
 languageSelection(language);
 $("#languageSwitch").addEventListener("click",(e) => {
-    e.target.value = e.target.value === "pt" ? "en" : "pt";
-    e.target.innerHTML = e.target.innerHTML === "Português" ? "English" : "Português";
     languageSelection(e.target.value);
+    if (e.target.value === "en"){
+        e.target.value = "pt"
+        e.target.innerHTML = "PT";
+    } else{
+        e.target.value = "en"
+        e.target.innerHTML = "EN";
+    }
 });
-// THEME:
 document.documentElement.setAttribute("data-theme", window.matchMedia('(prefers-color-scheme: light)').matches ? "light" : "dark");
 $("#themeSwitch").addEventListener("click",(e) => {
     const theme = document.documentElement.getAttribute("data-theme") === "dark" ? "light" : "dark";
