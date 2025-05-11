e99cf5974aa6a710b0a906b84d3a0e4fe2c4a7c2
viniciusmachadovianna <viniciusmachadovianna.dev@gmail.com>
2025-05-10 21:43:05 -0300

    style: shift var color of progress status on dark/light theme change

--- script.js
+++ script.js
@@ -5,19 +5,24 @@ const btnLanguage = document.getElementById('language'),
     projectsSection = document.getElementById('projects'),
     scrollProgress = document.getElementById('scrollProgress'),
     projectCount = document.querySelectorAll('article').length,
-    articles = document.querySelectorAll('article');
+    articles = document.querySelectorAll('article'),
+    progressBars = document.getElementById('progress').querySelector('div'),
+    progressSlash = document.getElementById('progressValue'),
+    projectDescriptions = document.querySelectorAll('.projectDescription');
      
 function setProjectCounter(){
     document.documentElement.style.setProperty('--projectCount', projectCount);
-    document.getElementById('progressValue').innerText = `0/${projectCount}`;
-    const projectList = document.getElementById('progress').querySelector('div');
+    progressSlash.textContent=`0/${projectCount}`;
+}
+
+
+function addProgressBars(){
     for (let i = 0; i < projectCount; i++) {
         const div = document.createElement('div');
         div.setAttribute('id',`bar${i+1}`);
-        projectList.appendChild(div);
+        progressBars.appendChild(div);
     }
 }
-
 function setArticlesColors(){
     const colors = {
         arariama:           ['#25344f','#b2becb','#632024'],
@@ -30,42 +35,6 @@ function setArticlesColors(){
         ticTacToe:          ['#bb00ff','#a40094','#800080'],
         youtubeConverter:   ['#9b1f00','#dcdcdc','#9b1f00'],
     };
-}
-
-  
-
-articles.forEach((el)=>{
-    const infoContainer = el.querySelector('.projectDescription').querySelector('.more')
-    el.querySelector('.projectDescription').addEventListener('click',()=>{
-        infoContainer.style.display = infoContainer.style.display === 'flex' ? 'none' : 'flex';
-        el.setAttribute('data-seen', 'true');
-        updateProjectsSeen();
-    })
-})
-
-   
-function changeLang() {
-    const docLang=document.documentElement.getAttribute("lang")==='pt'?'en':'pt';
-    document.documentElement.setAttribute("lang",docLang);
-    document.querySelectorAll("[data-lang]").forEach((element) => {
-        const key = element.getAttribute("data-lang");
-        element.innerHTML = lang[docLang][key] || lang['en'][key];
-    });
-}
-
-function updateProjectsSeen() {
-    const seenProjects = document.querySelectorAll('[data-seen="true"]'),
-
-    orange = getComputedStyle(document.documentElement).getPropertyValue('--orange').trim(),
-    green = getComputedStyle(document.documentElement).getPropertyValue('--green').trim();
-    document.getElementById('progressValue').innerText = `${seenProjects.length}/${projectCount}`;
-    document.querySelector(`#bar${seenProjects.length}`).style.backgroundColor = orange;
-    if (seenProjects.length === projectCount) {
-        document.getElementById('progressValue').style.color =  green
-        document.querySelectorAll('[id^="bar"]').forEach(bar => {
-            bar.style.backgroundColor = green;
-        });
-    }
 }
 
 function zigZagAlignArticles(){articles.forEach((p,i)=>{p.classList.toggle('right',i%2===1);})}
@@ -81,16 +50,46 @@ function changeTheme(){
     document.documentElement.setAttribute("data-theme", newTheme);
 }
 
+function changeLang() {
+    const newLang=document.documentElement.getAttribute("lang")==='pt'?'en':'pt';
+    document.documentElement.setAttribute("lang",newLang);
+    document.querySelectorAll("[data-lang]").forEach((element)=>{
+        const key=element.getAttribute("data-lang");
+        element.innerHTML=lang[newLang][key]||lang['pt'][key];
+    });
+}
+
+function toggleInfoVisibility(tgt){
+    const info = tgt.querySelector('.more');
+    info.style.display=info.style.display==='flex'?'none':'flex';
+    !tgt.parentNode.getAttribute('data-seen') && markProjectAsSeen(tgt.parentNode);
+}
+
+function markProjectAsSeen(proj){
+    proj.setAttribute('data-seen', 'true');
+    const seenProjects = document.querySelectorAll('[data-seen="true"]'),
+    orange = getComputedStyle(document.documentElement).getPropertyValue('--orange').trim(),
+    green = getComputedStyle(document.documentElement).getPropertyValue('--green').trim();
+    document.getElementById('progressValue').innerText = `${seenProjects.length}/${projectCount}`;
+    document.querySelector(`#bar${seenProjects.length}`).classList.add('seen');
+    if (seenProjects.length === projectCount) {
+        document.getElementById('progressValue').classList.add('completed');
+        document.getElementById('progress').querySelector('div').classList.add('completed');
+    }
+}
+
 function setupEventListeners(){
     window.addEventListener('wheel',handleScroll,{passive:false});
     btnTheme.addEventListener('click',changeTheme)
     btnLanguage.addEventListener('click',changeLang)
+    projectDescriptions.forEach((desc =>{desc.addEventListener('click',()=>{toggleInfoVisibility(desc)})}))
 }
 
 function init(){
     setupEventListeners();
-    setProjectCounter();
     zigZagAlignArticles();
+    setProjectCounter();
+    addProgressBars();
 }
 
 
